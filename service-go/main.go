88529275fda262afade3968653d46eb1db26e85f
monghithub <monghithub@gmail.com>
2026-02-28 01:04:32 +0100

    feat(#5): subtask 1 - Go module + main.go con health endpoint y router

---
+++ service-go/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+	})
+
+	log.Println("service-go starting on :11003")
+	log.Fatal(http.ListenAndServe(":11003", r))
+}
